1c2632051626e336a2fb737776b800424cd17971
Nathan Wang <110417475+nathanwang0114@users.noreply.github.com>
2022-08-28 03:30:03 +0000

    game mode

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ screenLength = 512
 
 screenWidth = 384
 
-pipe1 = pygame.Rect(512, -124, 95, 204)
+pipe1 = pygame.Rect(512, -94, 95, 204)
 
 pipe2 = pygame.Rect(512, 260, 95, 204)
 
-rect_player = pygame.Rect(30,180, 40, 40)
+rect_player = pygame.Rect(30,180, 39, 24)
 
 dimField = (screenLength, screenWidth)
 
@@ -20,7 +20,7 @@ screen = pygame.display.set_mode(dimField)
 
 player = pygame.image.load(os.path.join("assets", "fish.jpg")).convert()
 player.set_colorkey((255,255,255))
-player = pygame.transform.scale(player, (60, 60))
+player = pygame.transform.scale(player, (39, 24))
 
 
 background = pygame.image.load(os.path.join("assets", "background.png"))
@@ -29,10 +29,13 @@ dead = pygame.image.load(os.path.join("assets", "dead.png"))
 dead = pygame.transform.scale(dead, (257, 150))
 
 pipeDown = pygame.image.load(os.path.join("assets", "pipeDown.png"))
-pipeDown = pygame.transform.scale(pipeDown, (95, 204))
+pipeDown = pygame.transform.scale(pipeDown, (109, 234))
 
 pipeUp = pygame.image.load(os.path.join("assets", "pipeUp.png"))
-pipeUp = pygame.transform.scale(pipeUp, (95, 204))
+pipeUp = pygame.transform.scale(pipeUp, (109, 234))
+
+gameMode = pygame.image.load(os.path.join("assets", "gameMode.png"))
+gameMode = pygame.transform.scale(gameMode, (370, 70))
 
 
 
@@ -42,7 +45,7 @@ pipeUp = pygame.transform.scale(pipeUp, (95, 204))
 clock = pygame.time.Clock()
 
 pygame.font.init()
-font = pygame.font.SysFont("Comicsans", 50)
+font = pygame.font.SysFont("Comicsans", 30)
 
 #Game Loop
 running = True
@@ -68,17 +71,12 @@ while(running):
     
     clock.tick(FPS)
     screen.blit(background, (0,0))
-    screen.blit(player, rect_player)
-    screen.blit(pipeUp, pipe1)
-    # pygame.draw.rect(screen, (0, 200, 100), pipe1)
-    screen.blit(pipeDown, pipe2)
-    # pygame.draw.rect(screen, (0, 200, 100), pipe2)
     index = rect_player.collidelist([pipe1, pipe2])
+    if(not startState):
+        screen.blit(gameMode,(70, 100))
+        startText = font.render("press '1' for single player and '2' for two players", True, (0,0,0))
+        screen.blit(startText, (25,180))
 
-    text = font.render("SCORE: " + str(score) , True, (0,0,0))
-    screen.blit(text, (10,10))
-    
- 
     for event in pygame.event.get():
         #Quit game
         if event.type == pygame.KEYDOWN: 
@@ -89,6 +87,17 @@ while(running):
                 jumpState = True
                 gravity = False
                 jumpFrame = 0
+
+    if(startState):
+        screen.blit(background, (0,0))
+        screen.blit(player, rect_player)
+        screen.blit(pipeUp, pipe1)
+        # pygame.draw.rect(screen, (0, 200, 100), pipe1)
+        screen.blit(pipeDown, pipe2)
+        # pygame.draw.rect(screen, (0, 200, 100), pipe2)
+
+        text = font.render("SCORE: " + str(score) , True, (0,0,0))
+        screen.blit(text, (10,10))
 
 
     if jumpState == True:
@@ -111,9 +120,9 @@ while(running):
     if pipe1.left <= -75:
         score += 1
 
-        gap = random.randrange(0, 204)
-        pipe1 = pygame.Rect(512, -204 + gap, 95, gap)
-        pipe2 = pygame.Rect(512, gap + 180, 95, 384 - (gap + 180))
+        gap = random.randrange(0, 234)
+        pipe1 = pygame.Rect(512, -234 + gap, 95, 234)
+        pipe2 = pygame.Rect(512, gap + 150, 95, 384 - (gap + 150))
         print(gap)
 
 
